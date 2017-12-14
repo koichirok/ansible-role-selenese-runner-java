382785f75fabf9e01f6514f3bfe2700550340e5b
KIKUCHI Koichiro <koichiro@hataki.jp>
2017-12-14 11:48:52 +0900

    Add test for JAVA_VERSION line in selenese-runner script

--- tests/serverspec/freebsd_spec.rb
+++ tests/serverspec/freebsd_spec.rb
@@ -8,4 +8,15 @@ end
 describe file('/usr/local/bin/selenese-runner') do
   it { should be_file }
   it { should be_mode 555 }
+  its(:content) {
+    ver = Gem::Version.new(@selenese_runner_version)
+    if ver >= Gem::Version.new('3.0.0')
+      min_java_ver = '1.8'
+    elsif ver >= Gem::Version.new('2.0.0')
+      min_java_ver = '1.7'
+    else
+      min_java_ver = '1.6'
+    end
+    should match /^JAVA_VERSION="#{min_java_ver}\+"/
+  }
 end
